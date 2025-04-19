3f4e8aa1b4712f4f6ab9fb2e1b8881cbc66782b8
Shivam Sinha <78948969+superuser303@users.noreply.github.com>
2025-04-20 01:58:58 +0530

    Update main code.py

--- main code.py
+++ main code.py
@@ -12,6 +12,8 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.models import resnet50
 import cv2
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
