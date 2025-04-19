2d76eb8cb0bbfb8e24daf17a9fa741a6bb175871
Shivam Sinha <78948969+superuser303@users.noreply.github.com>
2025-04-20 01:09:16 +0530

    Update main code.py

--- main code.py
+++ main code.py
@@ -14,7 +14,7 @@ from torchvision.models import resnet50
 import cv2
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.image i…74151; line-height: 1.6;">
+from pytorch_grad_cam.utils.image i74151; line-height: 1.6;">
                 <li>Real-time plant identification and disease detection</li>
                 <li>Detailed preparation methods and medicinal uses</li>
                 <li>Disease severity assessment and remedies</li>
